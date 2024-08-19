1e898eb5d066141744fbfa04ee4b226da8352a67
zobekenobe <zobekenobe@gmail.com>
2024-08-20 00:09:45 +0100

    adding context to app

--- diabetes_app.py
+++ diabetes_app.py
@@ -12,6 +12,34 @@ model  = data[1]
 
 st.title("Diabetes Detector using Machine Learning")
 
+st.write("""This dataset is originally from the National Institute of Diabetes and Digestive and Kidney Diseases. The objective is to predict based on diagnostic measurements whether a patient has diabetes.
+Content
+
+Several constraints were placed on the selection of these instances from a larger database. In particular, all patients here are females at least 21 years old of Pima Indian heritage.
+
+    Pregnancies: Number of times pregnant
+    Glucose: Plasma glucose concentration a 2 hours in an oral glucose tolerance test
+    BloodPressure: Diastolic blood pressure (mm Hg)
+    SkinThickness: Triceps skin fold thickness (mm)
+    Insulin: 2-Hour serum insulin (mu U/ml)
+    BMI: Body mass index (weight in kg/(height in m)^2)
+    DiabetesPedigreeFunction: Diabetes pedigree function
+    Age: Age (years)
+    Outcome: Class variable (0 or 1)
+
+Sources:
+
+(a) Original owners: National Institute of Diabetes and Digestive and
+Kidney Diseases
+(b) Donor of database: Vincent Sigillito (vgs@aplcen.apl.jhu.edu)
+Research Center, RMI Group Leader
+Applied Physics Laboratory
+The Johns Hopkins University
+Johns Hopkins Road
+Laurel, MD 20707
+(301) 953-6231
+(c) Date received: 9 May 1990""")
+
 with st.form('myform'):
     pr = st.number_input(label = 'Pregnancies', min_value = 0, max_value = 10, value = 'min', step = 1)
     gu = st.number_input(label = 'Glucose mg/dL', min_value = 70, max_value = 199, step = 10, value = 'min')
